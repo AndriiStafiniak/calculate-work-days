62aa1a9be6ec6cac5ce62cc0fb6fb4993ca4df59
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-03-16 02:08:05 +0100

    added calculate function in js and padding css

--- script.js
+++ script.js
@@ -2,12 +2,33 @@
   const firstDate = document.querySelector(".js-firstDate");
   const secondtDate = document.querySelector(".js-secondDate");
 
+  //   const oneDay = 1000 * 60 * 60 * 24;
+  //   console.log(oneDay);
+
+  const calculateDaysAtWork = (fromDate, toDate) => {
+    console.log("wywolana");
+    let daysAtWork = 0;
+    let firstDate = new Date(fromDate);
+    while (firstDate <= new Date(toDate)) {
+      const day = firstDate.getDay();
+      if (day !== 0 && day !== 6) {
+        //0 - sobota, 6 - niedziela
+        daysAtWork++;
+      }
+      firstDate.setDate(firstDate.getDate() + 1);
+    }
+    return daysAtWork;
+  };
+
   const init = () => {
     const form = document.querySelector(".js-form");
     form.addEventListener("submit", (event) => {
       event.preventDefault();
-      console.log(firstDate.value);
-      console.log(secondtDate.value);
+      let fromDate = firstDate.value;
+      let toDate = secondtDate.value;
+      const result = calculateDaysAtWork(fromDate, toDate);
+
+      console.log(result);
     });
   };
   init();
